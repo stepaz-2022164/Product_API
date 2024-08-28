4995028d46529d675a9c89fca6ca6988e5f446de
stepaz-2022164 <stepaz-2022164@kinal.edu.gt>
2024-08-28 17:11:09 -0600

    Creacion del proyecto y modelos de datos

---
+++ Products_Server/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Products_Server.Models
+{
+    public class Product
+    {
+        public int Id { get; set; } //Propiedades publicas y aplicacion de los metodos get y set
+        [MaxLength(50, ErrorMessage =" El campo {0} debe contener {1} caractéres.")] //Atributos y/o metadatos
+        public string Name { get; set; } = null!;
+        [DataType(DataType.MultilineText)]
+        [MaxLength(500, ErrorMessage = "El campo {0} debe contener {1} caractéres.")]
+        public string Description { get; set; } = null!;
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Products_Server/Models/ProductsContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Products_Server.Models
+{
+    public class ProductsContext : DbContext //Herencia
+    {
+        
+    }
+}
